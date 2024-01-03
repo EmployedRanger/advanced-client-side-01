c7a067ad09310779c54eded28c3185c62d70512b
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-02 17:36:18 -0700

    Added event

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,6 +4,17 @@ let score = 0;
 const againButton = document.getElementsByClassName('again');
 const numberDisplay = document.getElementsByClassName('number');
 const scoreDisplay = document.getElementsByClassName('score');
-const highScoreDisplay = document.getElementsByClassName('highscore')
-const checkButton = document.getElementsByClassName('check')
-const 
+const highScoreDisplay = document.getElementsByClassName('highscore');
+const checkButton = document.getElementsByClassName('check');
+const message = document.getElementsByClassName('message');
+let guess = document.querySelector('.guess')
+
+
+let numberGuess = Math.floor(Math.random(1, 20) * 20);
+console.log(numberGuess);
+
+console.log(scoreDisplay)
+console.log(score)
+checkButton.addEventListener("click", () => {
+    let guess = document.querySelector('.guess');
+})
