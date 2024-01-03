cb5115c67a449d9a03fc88ec1adfa79331039837
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-03 15:06:51 -0700

    Added conditions

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,20 +1,34 @@
 'use strict';
 let highScore = 0;
 let score = 0;
-const againButton = document.getElementsByClassName('again');
-const numberDisplay = document.getElementsByClassName('number');
-const scoreDisplay = document.getElementsByClassName('score');
-const highScoreDisplay = document.getElementsByClassName('highscore');
-const checkButton = document.getElementsByClassName('check');
-const message = document.getElementsByClassName('message');
-let guess = document.querySelector('.guess')
+const againButton = document.querySelector('.again');
+let numberDisplay = document.querySelector('.number').textContent;
+const scoreDisplay = document.querySelector('.score').textContent;
+const highScoreDisplay = document.querySelector('.highscore');
+const checkButton = document.querySelector('.check');
+let message = document.querySelector('.message').textContent;
+let guess = document.querySelector('.guess').value;
 
 
 let numberGuess = Math.floor(Math.random(1, 20) * 20);
 console.log(numberGuess);
+console.log(message);
+console.log(guess);
 
-console.log(scoreDisplay)
-console.log(score)
 checkButton.addEventListener("click", () => {
-    let guess = document.querySelector('.guess');
+    let guess = document.querySelector('.guess').textContent;
+    console.log(guess);
+    if (guess > 20) {
+        message = 'Needs to be between 1-20';
+        console.log(message);
+    } else if (guess == numberGuess) {
+        message = 'Congrats! You win!';
+        console.log(message);
+        numberDisplay = numberGuess;
+        highScore = score;
+    } else {
+        message = 'Try again';
+        console.log(message);
+        score -= 1;
+    }
 })
