b7485256d3c13660669f806238e040b34489f409
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-03 15:35:57 -0700

    Can now play game

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,12 +1,12 @@
 'use strict';
 let highScore = 0;
-let score = 0;
+let score = 20;
 const againButton = document.querySelector('.again');
-let numberDisplay = document.querySelector('.number').textContent;
-const scoreDisplay = document.querySelector('.score').textContent;
+let numberDisplay = document.querySelector('.number');
+const scoreDisplay = document.querySelector('.score');
 const highScoreDisplay = document.querySelector('.highscore');
 const checkButton = document.querySelector('.check');
-let message = document.querySelector('.message').textContent;
+let message = document.querySelector('.message');
 let guess = document.querySelector('.guess').value;
 
 
@@ -16,19 +16,44 @@ console.log(message);
 console.log(guess);
 
 checkButton.addEventListener("click", () => {
-    let guess = document.querySelector('.guess').textContent;
-    console.log(guess);
-    if (guess > 20) {
-        message = 'Needs to be between 1-20';
-        console.log(message);
+    let guess = document.querySelector('.guess').value;
+    numberDisplay.textContent = guess;
+    if (guess > 20 || guess <= 0) {
+        message.textContent = 'Needs to be between 1-20';
+        console.log(message.textContent);
     } else if (guess == numberGuess) {
-        message = 'Congrats! You win!';
+        message.textContent = 'Congrats! You win!';
         console.log(message);
         numberDisplay = numberGuess;
+        CheckHighScore(highScore, score);
+    } else {
+        if (guess > numberGuess) {
+            message.textContent = 'Number is too high';
+        } else if (guess < numberGuess) {
+            message.textContent = 'Number is too low';
+        }
+        score -= 1;
+        DisplayScore(score);
+    }
+});
+
+againButton.addEventListener("click", () => {
+    score = 20;
+    guess = 0;
+    numberDisplay = '?';
+});
+
+function CheckHighScore (highScore, score) {
+    console.log('CheckHighScore ran');
+    if (score > highScore) {
         highScore = score;
     } else {
-        message = 'Try again';
-        console.log(message);
-        score -= 1;
+        score = highScore;
     }
-})
+    console.log('score is', score)
+    highScoreDisplay.textContent = score;
+} 
+
+function DisplayScore (score) {
+    scoreDisplay.textContent = score;
+}
