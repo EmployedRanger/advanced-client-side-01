edb781684b0f84cac40eaa2cb4e1c58de4a01f8d
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-02 17:23:48 -0700

    Added first const

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,9 @@
 'use strict';
-let 
+let highScore = 0;
+let score = 0;
+const againButton = document.getElementsByClassName('again');
+const numberDisplay = document.getElementsByClassName('number');
+const scoreDisplay = document.getElementsByClassName('score');
+const highScoreDisplay = document.getElementsByClassName('highscore')
+const checkButton = document.getElementsByClassName('check')
+const 
