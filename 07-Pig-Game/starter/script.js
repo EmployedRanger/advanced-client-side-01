df47ffb5fe81fdde179a6a6ecac56fa1d185c0c5
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-04 14:00:39 -0700

    Added hidden class

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
+
+diceElement.classList.add('hidden');
