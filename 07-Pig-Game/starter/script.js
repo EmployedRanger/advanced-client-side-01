7a993a01afda4c977f2b9ca11aa3cd0d46c21310
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-04 14:23:15 -0700

    Added switching player

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -8,8 +8,8 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0Element = document.getElementById('current--0');
 const current1Element = document.getElementById('current--1');
-const player0Element = document.querySelector('.player--0')
-const player1Element = document.querySelector('.player--1')
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 
 // Starting conditions
 score0Element.textContent = 0;
@@ -34,8 +34,9 @@ btnRoll.addEventListener('click', function () {
     } else {
         // Switch to next player
         document.getElementById(`current--${activePlayer}`).textContent = 0;
-
-        activePlayer = activePlayer === 0 ? 1: 0;
         currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1: 0;
+        player0Element.classList.toggle('player--active');
+        player1Element.classList.toggle('player--active');
     }
 });
