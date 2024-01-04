5508772d2b81bd8ee07ee895c20d51e1f86b0c88
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-04 14:19:49 -0700

    Added dice rolling

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,5 +3,39 @@
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
 const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+const current0Element = document.getElementById('current--0');
+const current1Element = document.getElementById('current--1');
+const player0Element = document.querySelector('.player--0')
+const player1Element = document.querySelector('.player--1')
 
+// Starting conditions
+score0Element.textContent = 0;
+score1Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+const scores = [0, 0];
+let activePlayer = 0;
+let currentScore = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+    const dice = Math.floor(Math.random() * 6) + 1;
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${dice}.png`;
+    console.log(dice);
+
+    if (dice !== 1) {
+        // Add dice to current score
+        currentScore += dice;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    } else {
+        // Switch to next player
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+
+        activePlayer = activePlayer === 0 ? 1: 0;
+        currentScore = 0;
+    }
+});
