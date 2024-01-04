9cbdb5aaa5fa6ebc4b78bd370e3907dc4f369962
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-03 18:29:39 -0700

    Finished

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,10 +4,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal)
 
 const openModal = function () {
-    console.log('Button clicked');
     modal.classList.remove('hidden');
     overlay.classList.remove('hidden');
 }
@@ -22,3 +20,11 @@ for (let i = 0; i < btnsOpenModal.length; i++)
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape') {
+        if (!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+    }
+});
