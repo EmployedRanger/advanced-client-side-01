80f0b67d51f2dba6dd0597d413c7e5895ec8d47f
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-10 16:25:15 -0700

    Updated notes

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -25,7 +25,36 @@ lufthansa.book(324, 'Logan Malmstrom');
 lufthansa.book(423, 'John Smith');
 
 const eurowings = {
-    name: 'Eurowings',
+    airline: 'Eurowings',
     iataCode: 'EW',
     bookings: [],
 };
+
+const swiss = {
+    airline: 'Swiss Air Lines',
+    iataCode: 'LX',
+    bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does NOT work
+// book(23, 'Sarah Williams');
+
+// Works perfectly
+book.call(eurowings, 238, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 231, 'yeeter');
+console.log(lufthansa);
+
+book.call(swiss, 543, 'Senya Ranger');
+console.log(swiss);
+
+// Apply method
+const flightData = [243, 'Jorge Cooper']
+book.apply(swiss, flightData);
+console.log(swiss)
+
+// Better method, use 'call()'
+book.call(swiss, ...flightData);
