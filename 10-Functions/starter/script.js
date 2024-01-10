27ab8007c247d66ee6d4713fdb1217490e42438b
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-10 16:08:18 -0700

    Added some notes

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,31 @@
 'use strict';
+
+
+const greet = function (greeting) {
+    return function (name) {
+        console.log(`${greeting} ${name}`);
+    };
+};
+greet('Yeet')("whipped")
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+greetArr('hi')('logan');
+
+const lufthansa = {
+    airline: 'Lufthansa',
+    iataCode: 'LH',
+    bookings: [],
+    book(flightNUm, name) {
+        console.log(`${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNUm}`);
+    this.bookings.push({ flight: `${this.iataCode}${flightNUm}`, name });
+    },
+};
+
+lufthansa.book(324, 'Logan Malmstrom');
+lufthansa.book(423, 'John Smith');
+
+const eurowings = {
+    name: 'Eurowings',
+    iataCode: 'EW',
+    bookings: [],
+};
