383a6cb9ba0443790c3df2d6b332ae416951cfa9
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-12 13:27:51 -0700

    Added answer

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -193,11 +193,13 @@
 // And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
 
 // GOOD LUCK 😀
-// */`
+// */
 
 (function () {
     const header = document.querySelector('h1');
     header.style.color = 'red';
+
+    document.querySelector('body').addEventListener('click', () => {
+        header.style.color = 'blue';
+    })
 })();
-
-
