dcc2ae0915904aa717387da64544997958619b65
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-01-25 14:18:48 -0700

    Added notes

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +(inputLoanAmount.value);
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -252,20 +252,24 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 // LECTURES
 
-console.log(0.1 + 0.2 === 3); // false
-console.log(Number('23')); 
-console.log(+'23'); // Short cut for Number, string to number
+// console.log(0.1 + 0.2 === 3); // false
+// console.log(Number('23')); 
+// console.log(+'23'); // Short cut for Number, string to number
 
 // Parsing
-console.log(Number.parseInt('30px')); // remove unnecessary things that aren't ##, but only from the front
-console.log(Number.parseInt('d333')); // Doeesn't work
+// console.log(Number.parseInt('30px')); // remove unnecessary things that aren't ##, but only from the front
+// console.log(Number.parseInt('d333')); // Doesn't work
 
-console.log(Number.parseInt('    2.5rem')); // 2
-console.log(Number.parseFloat('   2.5rem')); // 2.5
+// console.log(Number.parseInt('    2.5rem')); // 2
+// console.log(Number.parseFloat('   2.5rem')); // 2.5
 
-console.log(Number.isNaN(20)); // false
-console.log(Number.isNaN('20')); // false 
+// console.log(Number.isNaN(20)); // false
+// console.log(Number.isNaN('20')); // false 
 
-console.log((2.3).toFixed(0)); // 2
-console.log((2.3).toFixed(3)); // 2.300
+// toFixed makes it a string
+// console.log((2.3).toFixed(0)); // 2 as a string
+// console.log((2.3).toFixed(3)); // 2.300 as a string
 
+// 287,460,000,000
+const diameter = 287_640_000_000
+console.log(diameter);
