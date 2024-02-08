b7885d0230fa23356f8d5e2f235791bb7e21cbde
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-02-08 13:17:30 -0700

    Removed leaflet

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -15,7 +15,9 @@ if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(function (position) {
         const { latitude } = position.coords;
         const { longitude } = position.coords;
-        const map = L.map('map').setView([51.505, -0.09], 13);
+
+        const coords = [latitude, longitude]
+        const map = L.map('map').setView([coords], 13);
         
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
