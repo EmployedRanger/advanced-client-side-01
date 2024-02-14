5fee5819b99de06da3cf44c614c4ebc83456f02e
EmployedRanger <119915465+EmployedRanger@users.noreply.github.com>
2024-02-14 16:16:34 -0700

    Added final notes

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -5,9 +5,9 @@ console.log(price, tq);
 
 
 import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
+add('pizza', 8);
+add('chicken', 4);
+add('bananas', 7);
 
 const ShoppingCart2 = (function () {
     const cart = [];
@@ -66,3 +66,24 @@ console.log(stateDeepClone);
 if (module.hot) {
 module.hot.accept();
 }
+
+class Person {
+    #greeting = 'Hey';
+    constructor(name) {
+        this.name = name;
+        console.log(`${this.#greeting}, ${this.name}`);
+    }
+}
+
+const logan = new Person('Logan');
+
+console.log('Logan' ?? null);
+
+console.log(cart.find(el => el.quantity >= 2));
+
+Promise.resolve(('TEST').then(x => console.log(x);))
+
+import 'core-js/stable';
+
+import 'regenerator-runtime/runtime';
+
